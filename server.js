7f20c0d1ef24d4301b1c438e7bf043aa17275b6b
Dhruv <dhruvdev7794@gmail.com>
2018-08-01 09:02:28 -0400

    change origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin',
-        'http://localhost:4200');
+        'http://assignment5-wbdv-angular.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Origin, A-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods',
